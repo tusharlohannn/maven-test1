22b1487cd7af6f7558a75f3a8efe629f9283d8bd
Tushar Lohan <patbatemann18@gmail.com>
2024-09-16 16:30:47 +0530

    TusharL0001 - Wrote the code to check if a number is odd or even.

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.js
@@ -0,0 +1,14 @@
+function Tushar(){
+  console.log('I am Tushar');
+}
+
+function isEven(x){
+  if(x%2===0){
+    console.log('This number is Even!');
+  } else{
+  console.log('This number is Odd!');
+  }
+}
+
+Tushar();
+isEven(11);
